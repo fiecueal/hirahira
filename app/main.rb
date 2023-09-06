9e995943453eb299227366b11c642645d54555eb
fiecueal <fiecueal@proton.me>
2023-09-05 19:44:09 -0700

    update parameter values/names

--- app/main.rb
+++ app/main.rb
@@ -1,7 +1,7 @@
 GRAVITY = -0.01
 
 def launch args
-  peak = rand(250) + 300
+  peak = rand(180) + 360
   args.state.shoots << {
     x: rand(1081) + 100, # cus apparently mruby doesn't accept ranges for rand
     y: 0,
@@ -10,22 +10,24 @@ def launch args
     ttl: peak / 13,
     start: args.state.tick_count,
     peak: peak,
-    r: 222,
-    g: 222,
-    b: 222,
     primitive_marker: :sprite
   }
 end
 
 def tick_sparks args
   args.state.sparks.reverse_each do |spark|
-    spark.a = 111 * args.easing.ease(spark.start,
-                                     args.state.tick_count,
-                                     spark.ttl,
-                                     :flip)
-    args.state.sparks.delete spark if spark.a.zero?
-    spark.angle += spark.ttl
-    spark.y -= spark.ttl
+    percentage = args.easing.ease(spark.start,
+                                  args.state.tick_count,
+                                  spark.ttl,
+                                  :flip, :cube)
+
+    if percentage.zero?
+      args.state.sparks.delete spark
+      next
+    end
+
+    spark.a = 111 * percentage
+    spark.angle += percentage
   end
 end
 
@@ -34,17 +36,21 @@ def tick_shoots args
     shoot.y = shoot.peak * args.easing.ease(shoot.start,
                                             args.state.tick_count,
                                             shoot.ttl,
-                                            :flip, :quad, :flip)
-    args.state.shoots.delete shoot if shoot.y == shoot.peak
+                                            :flip, :cube, :flip)
+    if shoot.y == shoot.peak
+      args.state.shoots.delete shoot
+      next
+    end
+
     shoot.x += Math.cos(args.state.tick_count) * 3
-    3.times do |i|
+    11.times do |i|
       size = rand(20) + 10
       args.state.sparks << {
-        x: shoot.x + rand(7) - 3,
-        y: shoot.y,
+        x: shoot.x + rand(25) - 12,
+        y: shoot.y + rand(25) - 12,
         w: size,
         h: size,
-        ttl: 12,
+        ttl: shoot.ttl,
         start: args.state.tick_count,
         a: 111,
         angle: rand(360),
@@ -67,7 +73,7 @@ def tick args
   tick_shoots args
   tick_sparks args
 
-  launch args if args.state.sparks.empty?
+  launch args if args.state.shoots.empty?
 
   args.outputs.background_color = [ 11, 17, 23 ]
   args.outputs.sprites << [ args.state.sparks ]
